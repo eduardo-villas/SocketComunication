576fd4aca9d85964ec1d4757263135a0b042f2ef
eduardo.villasboas <eduardovillasboassantos@gmail.com>
2017-09-15 16:57:40 -0300

    testes
    teste

--- Client/src/main/java/br/uem/client/main/App.java
+++ Client/src/main/java/br/uem/client/main/App.java
@@ -1,6 +1,5 @@
 package br.uem.client.main;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -19,7 +18,7 @@ import br.uem.client.protocol.InvalidClientStateException;
 public class App {
 
 	private static final int SERVER_PORT = 3000;
-	private static final String SERVER_IP = "localhost";
+	private static final String SERVER_IP = "192.168.25.54";
 
 	private static Logger logger = Logger.getLogger(App.class);
 
@@ -30,38 +29,66 @@ public class App {
 			@Override
 			public void execute(Client client) throws Exception {
 				
-				final int SIZE_MESSAGE = 8*8;
+				final int transferInMB = 500;
+				sendBytes(client, 32, 1, transferInMB);
+				sendBytes(client, 32, 2, transferInMB);
+				sendBytes(client, 32, 4, transferInMB);
+				sendBytes(client, 32, 8, transferInMB);
+				sendBytes(client, 32, 16, transferInMB);
+				
+				sendBytes(client, 64, 1, transferInMB);
+				sendBytes(client, 64, 2, transferInMB);
+				sendBytes(client, 64, 4, transferInMB);
+				sendBytes(client, 64, 8, transferInMB);
+				sendBytes(client, 64, 16, transferInMB);
+
+				sendBytes(client, 128, 1, transferInMB);
+				sendBytes(client, 128, 2, transferInMB);
+				sendBytes(client, 128, 4, transferInMB);
+				sendBytes(client, 128, 8, transferInMB);
+				sendBytes(client, 128, 16, transferInMB);
+				
+			}
+
+			private void sendBytes(Client client, final int SIZE_MESSAGE, final int multiplyPacket, final int transferInMB) throws IOException {
+				
 				String message = String.join("", Collections.nCopies(SIZE_MESSAGE/8, "8bytesMS"));;
-				final int SIZE_PACKAGE = 1024*4;
+				final int SIZE_PACKAGE = 1024*multiplyPacket;
 				final String TAG = "packet"+SIZE_PACKAGE; 
 				ElapTime elapTime = new ElapTime();
+				//(1638400 *  32) / (1024*1024) para mensagens de 32 bytes 50mb
+//				final int TOTAL_MESSAGES = 1638400;
+				final int TOTAL_MESSAGES = (transferInMB * (1024*1024)) / SIZE_MESSAGE;
+				final int MB_TRANSFER = (TOTAL_MESSAGES *  SIZE_MESSAGE) / (1024*1024);// QUANTIDADE EM MB para mensagens de 32 bytes 50mb
 				
-				final int TOTAL_MESSAGES = 500000;
-				
-				PrintWriter printWriter = new PrintWriter(new FileWriter("stats.txt", true));
-				
-				long timeWithoutPacket = 0 ;
+				PrintWriter printWriter = new PrintWriter(new FileWriter("stats.log", true));
+				ElapTime elapTimeTotal = new ElapTime();
+				long timeTransferWithoutPacket = 0 ;
+				elapTimeTotal.start();
 				for (int i = 0 ;i < TOTAL_MESSAGES; ++i) {
-					logger.info(String.format("enviando mensagem %d: %s", i+1, message));
+					logger.info(String.format("enviando mensagem sem agregação %d: %s", i+1, message));
 					elapTime.start();
 					client.sendMessage(message);
 					elapTime.finish();
-					timeWithoutPacket += elapTime.elapTime();
+					timeTransferWithoutPacket += elapTime.elapTime();
 				}
 				elapTime.finish();
-				logger.info(String.format("tempo de transmissao sem agregacao TAG:%s %d",TAG, timeWithoutPacket));
+				elapTimeTotal.finish();
+				final long totalTimeWithoutPacket = elapTimeTotal.elapTime();
+				logger.info(String.format("tempo de transmissao sem agregacao TAG:%s %d",TAG, timeTransferWithoutPacket));
 				
-				long timeWithPacket = 0;
+				long timeTransferWithPacket = 0;
+				elapTimeTotal.start();
 				StringBuilder sb = new StringBuilder();
 				for (int i = 0 ;i < TOTAL_MESSAGES; ++i) {
-					logger.info(String.format("enviando mensagem %d: %s", i+1, message));
+					logger.info(String.format("enviando mensagem com agregação %d: %s", i+1, message));
 					sb.append(message);
 					if (sb.length() >= SIZE_PACKAGE) {
 						logger.info(String.format("Tamanho do pacote enviado %s", sb.length()));
 						elapTime.start();
 						client.sendMessage(sb.toString());
 						elapTime.finish();
-						timeWithPacket += elapTime.elapTime();
+						timeTransferWithPacket += elapTime.elapTime();
 						sb = new StringBuilder();
 					}
 				}
@@ -70,18 +97,31 @@ public class App {
 					elapTime.start();
 					client.sendMessage(sb.toString());
 					elapTime.finish();
-					timeWithPacket += elapTime.elapTime();
+					timeTransferWithPacket += elapTime.elapTime();
 				}
+				elapTimeTotal.finish();
+				final long totalTimeWithPacket = elapTimeTotal.elapTime();
 				
 				List<String> stats;
-				long perc = ((timeWithPacket/timeWithoutPacket)*100);
-				stats = Lists.newArrayList("Teste",
+
+				stats = Lists.newArrayList("\nTeste",
+						
 						String.format("Quantidade de mensagens %s.", TOTAL_MESSAGES),
 						String.format("Tamanho das mensagens %s bytes", SIZE_MESSAGE),
 						String.format("Tamanho dos pacotes %s bytes", SIZE_PACKAGE),
-						String.format("Tempo de transmissão em milisegundos sem agregação %s", ElapTime.toMiliseconds(timeWithoutPacket)),
-						String.format("Tempo de transmissão em milisegundos com agregação %s", ElapTime.toMiliseconds(timeWithPacket)),
-						String.format("Percentual de  %f%5", perc)
+						String.format("Total transferido %s MB", MB_TRANSFER),
+						
+						String.format("Tempo de transmissão em milisegundos sem agregação   %s", ElapTime.toMiliseconds(timeTransferWithoutPacket)),
+						String.format("Tempo de transmissão em nanosegundos sem agregação   %s", timeTransferWithoutPacket),
+						String.format("Tempo total em milisegundos sem agregação            %s", ElapTime.toMiliseconds(totalTimeWithoutPacket)),
+						String.format("Tempo total em nanosegundos sem agregação            %s", totalTimeWithoutPacket),
+						String.format("Tempo de transmissão em milisegundos com agregação   %s", ElapTime.toMiliseconds(timeTransferWithPacket)),
+						String.format("Tempo de transmissão em nanosegundos com agregação   %s", timeTransferWithPacket),
+						String.format("Tempo de total em milisegundos com agregação         %s", ElapTime.toMiliseconds(totalTimeWithPacket)),
+						String.format("Tempo de total em nanosegundos com agregação         %s", totalTimeWithPacket),
+						
+						String.format("Percentual transferencia                            %4.3f", ((new Double(timeTransferWithPacket)/new Double(timeTransferWithoutPacket) * 100) - 100)),
+						String.format("Percentual total                                    %4.3f", ((new Double(totalTimeWithPacket)/new Double(totalTimeWithoutPacket) * 100) - 100))
 						);
 				
 				stats.stream().map(a -> a + "\n").forEach(printWriter::write);
@@ -89,8 +129,7 @@ public class App {
 				printWriter.flush();
 				printWriter.close();
 				elapTime.finish();
-				logger.info(String.format("tempo de transmissao com agregacao TAG:%s %d",TAG, timeWithPacket));
-				
+				logger.info(String.format("tempo de transmissao com agregacao TAG:%s %d",TAG, timeTransferWithPacket));
 			}
 
 		};
