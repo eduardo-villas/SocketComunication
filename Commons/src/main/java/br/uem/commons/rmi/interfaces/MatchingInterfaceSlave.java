9ac802900af5bee92be1b86eb7e2faadfd1f1e40
Unknown <eduardo.dabrisa@gmail.com>
2018-09-24 22:13:09 -0300

    Alterada interface rmi para receber uma string.
    Alterada interface rmi para receber uma string.

--- Commons/src/main/java/br/uem/commons/rmi/interfaces/MatchingInterfaceSlave.java
+++ Commons/src/main/java/br/uem/commons/rmi/interfaces/MatchingInterfaceSlave.java
@@ -1,10 +1,9 @@
 package br.uem.commons.rmi.interfaces;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Hashtable;
 
 public interface MatchingInterfaceSlave extends Remote {     
 	public void receive(String messageRequisition) throws RemoteException, Exception;
-    public void receive_slave_SR(Integer size, String idThread, String ip, long id, String option, String root, Integer group, Hashtable<String,String> subject, Hashtable<String,String> object, Hashtable<String,String> result, Hashtable<String,String> border) throws RemoteException, Exception;  
+    public void receive_slave_SR(String IntermediaryResult) throws Exception;  
 }
 
