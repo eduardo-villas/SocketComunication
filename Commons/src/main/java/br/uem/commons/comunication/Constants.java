f07086b4fde69f9692542876667c9d47d7231eb4
Unknown <eduardo.dabrisa@gmail.com>
2018-09-28 16:14:42 -0300

    construtor padrão encapsulado.
    Criado construtor padrão da classe Constants para que ninguém consiga instancia-la.

--- Commons/src/main/java/br/uem/commons/comunication/Constants.java
+++ Commons/src/main/java/br/uem/commons/comunication/Constants.java
@@ -1,11 +1,15 @@
 package br.uem.commons.comunication;
 
 public class Constants {
-
+	
 	public static final String GOODBYE = "GOODBYE";
 	public static final String HEY = "HEY\n";
 	public static final int HEADER_SIZE = 32;
 
+	private Constants() {
+		
+	}
+	
 	public static boolean isGoodbye(String message) {
 		return (Constants.GOODBYE+"\n").equals(message);
 	}
