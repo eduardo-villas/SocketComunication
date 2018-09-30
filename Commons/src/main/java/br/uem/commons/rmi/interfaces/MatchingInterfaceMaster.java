7b448e9a2cae34a6f27e52db9ccd4c20e88fa662
Unknown <eduardo.dabrisa@gmail.com>
2018-09-30 19:09:39 -0300

    Remoção das intefaces do rmi
    Removendo interfaces do rmi

--- Commons/src/main/java/br/uem/commons/rmi/interfaces/MatchingInterfaceMaster.java
+++
@@ -1,8 +0,0 @@
-package br.uem.commons.rmi.interfaces;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MatchingInterfaceMaster extends Remote {     
-    public void result(String message) throws RemoteException, Exception;
-}
-
--- Commons/src/main/java/br/uem/commons/rmi/interfaces/MatchingInterfaceSlave.java
+++
@@ -1,9 +0,0 @@
-package br.uem.commons.rmi.interfaces;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface MatchingInterfaceSlave extends Remote {     
-	public void receive(String messageRequisition) throws RemoteException, Exception;
-    public void receive_slave_SR(String IntermediaryResult) throws Exception;  
-}
-
